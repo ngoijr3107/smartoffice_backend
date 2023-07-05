07b95206ce0a5c346e4d470e2e618374e10184e4
Paschal Ngoi <47353288+ngoijr3107@users.noreply.github.com>
2023-07-06 00:45:39 +0300

    initial commit

---
+++ database/migrations/2023_07_02_183622_create_visitors_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('visitors', function (Blueprint $table) {
+            $table->id();
+            $table->string('fname');
+            $table->string('lname');
+            $table->date('checkintime');
+            $table->date('checkouttime')->nullable();
+            $table->string('email');
+            $table->string('phone');
+            $table->string('visit_purpose');
+            $table->string('department');
+            $table->string('status')->default('in');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('visitors');
+    }
+};
